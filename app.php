eec751a93985052cd097db7adf70b82df5528aa0
Rukiye Çadırcı <83542142+rukicad@users.noreply.github.com>
2021-08-18 11:03:27 +0300

    Update app.php
    Homework has been uploaded to the system.

--- app.php
+++ app.php
@@ -1,36 +1,49 @@
 <?php
-
-$gender = "male"; // cinsiyet (male/female)
-$weight = 55; // kilo (kg)
-$height = 170; // boy (cm)
-$age = 25; // yaş (sene)
+$gender = "female"; // Cinsiyet
+$weight = 55; // Kilo 
+$height = 160; // Boy
+$age = 23; // Yaş
 
 $guess = "1470"; // Tahmin edilen değer
+	$guess = (int) $guess; // Cast to integer
+	
+      if($gender == "female"){ // Cinsiyetin kadın olması durumu
+	
+
+	    $female_bmr = 75.362 + (50.397 * $weight) + (6.799 * $height) - (7.677 * $age); // bmr (female)
+	    
+	    echo "BMR: " . $female_bmr ;
+	    echo "<br>";
+	
+
+	    if ($female_bmr < $guess) {
+	        echo "Tahmin değerinden düşük";
+	    } elseif ($female_bmr == $guess){
+	        echo "Tahmin değerine eşit";
+	    } else {
+	        echo "Tahmin değerinden büyük";
+	    }
+	} else { // Cinsiyetin erkek olması durumu
+	
+
+	    $male_bmr = 347.593 + (8.247 * $weight) + (4.098 * $height) - (5.330 * $age); // bmr (male)
+	    
+		echo "BMR: " . $male_bmr;
+		echo "<br>";
+	
+
+	    if ($male_bmr < $guess) {
+	        echo "Tahmin değerinden düşük";
+	    } elseif ($male_bmr == $guess){
+	        echo "Tahmin değerine eşit";
+	    } else {
+	        echo "Tahmin değerinden büyük";
+	    }
+	}
+	
+
 
 /**
- * Bu ödevde bazal metabolizma hesaplaması yapacağız.
- * Yukarıdaki değişkenleri ve aşağıdaki formülü kullanarak
- * kişinin günlük BMR ihtiyacını hesaplayıp
- * bunu ekrana yazan ve tahmin değeri ile karşılaştıran
- * PHP kodunu oluşturmamız gerekiyor.
- * 
  * Erkekler için; 88.362 + (13.397 x kilo) + (4.799 x boy) – (5.677 x yaş)
  * Kadınlar için; 447.593 + (9.247 x kilo) + (3.098 x boy) – (4.330 x yaş)
- * 
- * Tahmin değeri ile karşılaştırmanızın sonucunda
- * "Tahmin değerinden düşük", "Tahmin değerinden büyük" veya
- * "Tahmin değerine eşit" şeklinde çıktı vermeniz gerekiyor.
- * 
- * Örneğin;
- * $gender = "female";
- * $weight = 60;
- * $height = 170;
- * $age = 30;
- * 
- * Ekrana
- * 
- * BMR: 1399.173
- * Tahmin değerinden düşük
- * 
- * yazması gerekiyor.
  */
